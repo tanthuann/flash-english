b4e3fa375705df5911138cfa8a60248d38be1578
tanthuann <truongtanthuan99@gmail.com>
2023-04-24 21:52:38 +0700

    feat: add ProvideFamiliesContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import Card from "./components/Card";
 import "./App.css";
+import { ProvideFamilies } from "./contexts/ProvideFamilies";
 
 const App = () => {
   return (
     <div className="App">
-      <main className="main">
-        <Card />
-      </main>
+      <ProvideFamilies>
+        <main className="main">
+          <Card />
+        </main>
+      </ProvideFamilies>
     </div>
   );
 };
