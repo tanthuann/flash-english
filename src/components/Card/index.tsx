679669677e3b99d0d6d40ec55602674b3aa9792b
tanthuann <truongtanthuan99@gmail.com>
2023-04-25 22:53:22 +0700

    feat: get crawl data from my server

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,48 +1,18 @@
 // libs
 import { SoundFilled } from "@ant-design/icons";
 import { Row, Col } from "antd";
+// contexts
+import { useFamilies } from "../../contexts/ProvideFamilies";
 // others
 import "./style.scss";
 
-const wordInfo = {
-  word: "Hello",
-  pos: " exclamation, noun",
-  phon: "həˈləʊ",
-  audio:
-    "https://www.oxfordlearnersdictionaries.com/media/english/us_pron/h/hel/hello/hello__us_1_rr.mp3",
-  senses: [
-    {
-      sense:
-        "used as a greeting when you meet somebody, in an email, when you answer the phone or when you want to attract somebody’s attention",
-      examples: ["Hello John, how are you?", "Hello, is there anybody there?"]
-    },
-    {
-      sense: "used to show that you are surprised by something",
-      examples: ["Hello, hello, what's going on here?"]
-    }
-  ],
-  ref: [
-    {
-      engine: "GG Translate",
-      href: "https://translate.google.com/?hl=vi&sl=en&tl=vi&text=hello&op=translate"
-    },
-    {
-      engine: "Glosbe",
-      href: "https://vi.glosbe.com/en/vi/hello"
-    },
-    {
-      engine: "Oxford",
-      href: "https://www.oxfordlearnersdictionaries.com/definition/english/hello"
-    }
-  ]
-};
-
 const Card = () => {
-  const { audio, word, pos, senses, ref, phon } = wordInfo;
+  const { wordInfo } = useFamilies();
+  const { audio, word, pos, senses, ref, phon } = wordInfo || {};
 
   return (
     <div className="card-wrapper">
-      <Row gutter={[16, 16]} justify="start" align="middle">
+      <Row hidden={!wordInfo} gutter={[16, 16]} justify="start" align="middle">
         <Col className="sound" span={2}>
           <SoundFilled
             onClick={() => {
@@ -51,7 +21,7 @@ const Card = () => {
           />
         </Col>
         <Col className="phon" span={22}>
-          /{phon}/
+          {phon}
         </Col>
         <Col className="word" span={24}>
           {word}
@@ -61,7 +31,7 @@ const Card = () => {
         </Col>
         <Col className="explains" span={24}>
           <ol className="senses">
-            {senses.map(({ sense, examples }) => (
+            {senses?.map(({ sense, examples }) => (
               <li key={sense} className="sense">
                 {sense}
                 <ul className="examples">
@@ -77,7 +47,7 @@ const Card = () => {
         </Col>
         <Col className="ref" span={24}>
           <Row justify="center" align="middle">
-            {ref.map(({ engine, href }) => (
+            {ref?.map(({ engine, href }) => (
               <Col key={engine} span={6}>
                 <a target="_blank" rel="noreferrer" href={href}>
                   {engine}
--- src/contexts/ProvideFamilies/index.tsx
+++ src/contexts/ProvideFamilies/index.tsx
@@ -1,6 +1,8 @@
 // libs
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import axios from "axios";
 // types
+import type { WordInfo } from "../../types";
 import type { PropsWithChildren } from "react";
 // data
 import families from "./families.json";
@@ -8,12 +10,16 @@ import families from "./families.json";
 export const ProvideFamiliesContext = createContext<{
   families: string[][];
   randomWord: () => string;
+  wordInfo?: WordInfo;
 }>({
   families,
-  randomWord: () => ""
+  randomWord: () => "",
+  wordInfo: undefined
 });
 
 export const ProvideFamilies = ({ children }: PropsWithChildren) => {
+  const [wordInfo, setWordInfo] = useState();
+
   const randomWord = () => {
     const familiesWord = families[Math.floor(Math.random() * families.length)];
 
@@ -22,8 +28,23 @@ export const ProvideFamilies = ({ children }: PropsWithChildren) => {
     return word;
   };
 
+  const crawlData = async () => {
+    try {
+      const word = randomWord();
+      const res = await axios.get(`https://fidbsd-8080.csb.app/api/${word}`);
+      setWordInfo(res.data);
+    } catch (error) {
+      await crawlData();
+    }
+  };
+
+  useEffect(() => {
+    crawlData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <ProvideFamiliesContext.Provider value={{ families, randomWord }}>
+    <ProvideFamiliesContext.Provider value={{ families, randomWord, wordInfo }}>
       {children}
     </ProvideFamiliesContext.Provider>
   );
