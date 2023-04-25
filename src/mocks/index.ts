fc928ce7ee90fe5fbbbcdcebfe0d959b16a855c7
tanthuann <truongtanthuan99@gmail.com>
2023-04-25 22:52:34 +0700

    chore: save the mock

---
+++ src/mocks/index.ts
@@ -0,0 +1,32 @@
+export const wordInfo = {
+  word: "Hello",
+  pos: " exclamation, noun",
+  phon: "həˈləʊ",
+  audio:
+    "https://www.oxfordlearnersdictionaries.com/media/english/us_pron/h/hel/hello/hello__us_1_rr.mp3",
+  senses: [
+    {
+      sense:
+        "used as a greeting when you meet somebody, in an email, when you answer the phone or when you want to attract somebody’s attention",
+      examples: ["Hello John, how are you?", "Hello, is there anybody there?"]
+    },
+    {
+      sense: "used to show that you are surprised by something",
+      examples: ["Hello, hello, what's going on here?"]
+    }
+  ],
+  ref: [
+    {
+      engine: "GG Translate",
+      href: "https://translate.google.com/?hl=vi&sl=en&tl=vi&text=hello&op=translate"
+    },
+    {
+      engine: "Glosbe",
+      href: "https://vi.glosbe.com/en/vi/hello"
+    },
+    {
+      engine: "Oxford",
+      href: "https://www.oxfordlearnersdictionaries.com/definition/english/hello"
+    }
+  ]
+};
